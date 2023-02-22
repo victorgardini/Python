82cb245db566f6c5d7183c3f881e3f3f978542d8
Victor Fernandes Gardini <victor.fernandes@resh.com.br>
2023-02-22 14:28:57 -0300

    Implemented Compress and Uncompress String

---
+++ arrays_strings/compress_strings/compres.py
@@ -0,0 +1,86 @@
+"""
+Challenge: Compress a string such that 'AAABCCDDDDE' becomes 'A3BC2D4E'.
+Only compress the string if it saves space.
+
+Solution: Iterate through the string and keep a count of the current character.
+If the next character is different than the current character, append the current
+and its count to the compressed string. If the compressed string is
+longer than the original string, return the original string.
+
+Some things to note:
+https://stackoverflow.com/questions/4435169/how-do-i-append-one-string-to-another-in-python/4435752#4435752
+"""
+from typing import Tuple
+
+
+class CompressedString:
+    def __init__(self, string: str):
+        self.string, self._is_compressed = self.compress_string(string)
+
+    @staticmethod
+    def compress_string(string: str) -> Tuple[str, bool]:
+        """
+        Compress a string such that 'AAABCCDDDD' becomes 'A3BC2D4'.
+        Only compress the string if it saves space.
+        """
+        if not string:
+            return "", False
+
+        compressed_string = ""
+        current_char = string[0]
+        count = 0
+        for char in string:
+            if char == current_char:
+                count += 1
+            else:
+                if count > 1:
+                    compressed_string += f"{current_char}{count}"
+                else:
+                    compressed_string += current_char
+                count = 1
+                current_char = char
+
+        if count > 1:
+            compressed_string += f"{current_char}{count}"
+        else:
+            compressed_string += current_char
+
+        if len(compressed_string) >= len(string):
+            return string, False
+        else:
+            return compressed_string, True
+
+    @staticmethod
+    def uncompress_string(string: str) -> str:
+        """
+        Uncompress a string such that 'A3BC2D4' becomes 'AAABCCDDDD'.
+        """
+        uncompressed_string = ""
+        count = 0
+
+        for i in range(len(string) - 1, -1, -1):
+            if string[i].isdigit():
+                count += int(string[i])
+            else:
+                if count == 0:
+                    uncompressed_string += string[i]
+                else:
+                    uncompressed_string += string[i] * count
+                count = 0
+        return uncompressed_string[::-1]
+
+    def is_compressed(self) -> bool:
+        return self._is_compressed
+
+    def __str__(self) -> str:
+        return self.string
+
+    def __repr__(self) -> str:
+        return f'CompressedString(string="{self.string}", is_compressed={self._is_compressed})'
+
+
+if __name__ == "__main__":
+    # print(compress_string("AAABCCDDDDE"))
+    new_string = CompressedString("AAABCCDDDDE")
+
+    print(new_string.__repr__())
---
+++ arrays_strings/compress_strings/test_compressed_strings.py
@@ -0,0 +1,25 @@
+import pytest
+from compres import CompressedString
+
+
+@pytest.mark.parametrize("string, expected", [
+    ("AABBCC", "AABBCC"),
+    ("AAABCCDDDDE", "A3BC2D4E"),
+    ("BAAACCDDDD", "BA3C2D4"),
+    ("AAABAACCDDDD", "A3BA2C2D4"),
+    ("A", "A"),
+    ("", ""),
+])
+def test_compress_string(string, expected):
+    assert CompressedString(string).string == expected
+
+
+@pytest.mark.parametrize("string, expected", [
+    ("A3BC2D4E", "AAABCCDDDDE"),
+    ("BA3C2D4", "BAAACCDDDD"),
+    ("A3BA2C2D4", "AAABAACCDDDD"),
+    ("A", "A"),
+    ("", ""),
+])
+def test_uncompress_string(string, expected):
+    assert CompressedString.uncompress_string(string) == expected

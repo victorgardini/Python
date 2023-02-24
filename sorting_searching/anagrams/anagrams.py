878d915b26f44185c4cdf8cb43965b9a96e2be3c
Victor Fernandes Gardini <victor.fernandes@resh.com.br>
2023-02-24 11:25:31 -0300

    Fixed anagrams code

--- sorting_searching/anagrams/anagrams.py
+++ sorting_searching/anagrams/anagrams.py
@@ -4,7 +4,7 @@ from collections import OrderedDict
 from typing import List
 
 
-def group_anagrams(words: List[str]) -> List[List[str]]:
+def group_anagrams(words: List[str]) -> List[str]:
     """Group anagrams together in a list of words."""
 
     # Create a dictionary of anagrams
@@ -13,13 +13,13 @@ def group_anagrams(words: List[str]) -> List[List[str]]:
     for word in words:
         # Use a tuple, which is hashable and serves as the key in anagram_map
         sorted_chars = tuple(sorted(word))  # ex: ('a', 'm', 'r')
-        if sorted_chars in anagram_dict:
-            anagram_dict[sorted_chars].append(word)
+        if sorted_chars in anagram_dict:  # ex: ('a', 'm', 'r') in anagram_map
+            anagram_dict[sorted_chars].append(word)  # ex: anagram_map[('a', 'm', 'r')].append('arm')
         else:
-            anagram_dict[sorted_chars] = [word]
+            anagram_dict[sorted_chars] = [word]  # ex: anagram_map[('a', 'm', 'r')] = ['arm']
 
     result = []
-    for anagram in anagram_dict.values():
-        result.extend(anagram)
+    for anagram in anagram_dict.values():  # ex: anagram_map.values() = [['arm'], ['ram'], ['mar']]
+        result.extend(anagram)  # ex: result.extend(['arm'])
 
-    return result
+    return result  # ex: ['arm', 'ram', 'mar']
